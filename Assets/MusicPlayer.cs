b5ad34a20a87ab19592bdf27c04e19945cc24eb5
Frédéric Darré <frederic.darre@outlook.com>
2020-05-26 21:08:54 +0200

    Make the music keep playing in level 1

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+        void Start()
+    {
+        Invoke("StartGame", 5f);
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void Update()
+    {
+        
+    }
+}
