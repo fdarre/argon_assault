a5d8ff31d3dbf962c91a1f2ae7ee77e1bdf5c81f
Frédéric Darré <frederic.darre@outlook.com>
2020-06-14 21:18:23 +0200

    Delete unnecessary print statement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -70,7 +70,6 @@ public class PlayerController : MonoBehaviour
     //called by string reference
     private void OnPlayerDeath()
     {
-        print("controls frozen");
         isControlEnabled = false;
     }
 
