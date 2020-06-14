1cbd9a4742902b01f3427090fa490dccda2e284d
Frédéric Darré <frederic.darre@outlook.com>
2020-06-14 09:56:20 +0200

    Create script to handle player collisions

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartDeathSequence();
+    }
+
+    private void StartDeathSequence()
+    {
+        print("player dying");
+        SendMessage("OnPlayerDeath");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
